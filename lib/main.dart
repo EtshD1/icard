8ae9796b77c382e1c63f865791b3dda5951066bd
Etsh <H79Pain@gmail.com>
2020-06-23 17:19:35 +0200

    created materialapp with a colored background

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,4 @@
+import 'package:flutter/src/widgets/binding.dart';
+import 'src/icard.dart';
 
+void main() => runApp(ICard());
---
+++ lib/src/icard.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ICard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.indigo,
+        body: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
